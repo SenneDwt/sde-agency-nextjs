d4e58966cb3ba3d8437c23e98b9593b803355b25
Senne Dwt <Sennedewit16@icloud.com>
2025-12-24 07:54:23 +0100

    layout teruggezet

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,16 @@ export default function RootLayout({
   return (
     <html lang="nl">
       <head>
-        {/* Google Consent Mode - Default State (VOOR Cookiebot) */}
+        {/* Cookiebot */}
+        <script 
+          id="Cookiebot" 
+          src="https://consent.cookiebot.com/uc.js" 
+          data-cbid="c4718494-9b1c-4ca2-9c50-a1c2678aa33a" 
+          type="text/javascript"
+          async
+        />
+        
+        {/* Google Consent Mode - Default State */}
         <script
           data-cookieconsent="ignore"
           dangerouslySetInnerHTML={{
@@ -34,9 +43,9 @@ export default function RootLayout({
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('consent', 'default', {
+  'ad_personalization': 'denied',
   'ad_storage': 'denied',
   'ad_user_data': 'denied',
-  'ad_personalization': 'denied',
   'analytics_storage': 'denied',
   'functionality_storage': 'denied',
   'personalization_storage': 'denied',
@@ -44,23 +53,15 @@ gtag('consent', 'default', {
   'wait_for_update': 500,
 });
 gtag("set", "ads_data_redaction", true);
-gtag("set", "url_passthrough", true);
+gtag("set", "url_passthrough", false);
             `,
           }}
         />
-
-        {/* Cookiebot */}
-        <script 
-          id="Cookiebot" 
-          src="https://consent.cookiebot.com/uc.js" 
-          data-cbid="c4718494-9b1c-4ca2-9c50-a1c2678aa33a"
-          data-blockingmode="auto"
-          type="text/javascript"
-        />
         
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager - geblokkeerd tot cookie toestemming */}
         <script
-          data-cookieconsent="ignore"
+          type="text/plain"
+          data-cookieconsent="statistics"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -73,13 +74,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) - geblokkeerd tot cookie toestemming */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-NRJCCGSD"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
+            data-cookieconsent="statistics"
           />
         </noscript>
         <ScrollToTop />
